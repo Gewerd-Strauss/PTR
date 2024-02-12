1d73ce42ccdb1e05d8c237ee68496380d951964a
Gewerd-Strauss <80548781+Gewerd-Strauss@users.noreply.github.com>
2024-02-12 18:49:59 +0100

    update tests

--- tests/testthat/test-get_boards2.R
+++ tests/testthat/test-get_boards2.R
@@ -1,27 +1,12 @@
-test_that("testing get_Boards2", {
-    board30x60_pot12x10 <- get_boards2(30, 60, NA, NA, 12, 10, 17, "rectangle")
-    board30x60_pot10x12 <- get_boards2(30, 60, NA, NA, 10, 11, 17, "rectangle")
-    expect_true(length(board30x60_pot10x12$board_1$pot_centers)>length(board30x60_pot12x10$board_1$pot_centers))
-    # {
-    # return(PTR_generateBoardLayouts2(
-    #     pots = 17,
-    #     board_width = 30,
-    #     board_height = 60,
-    #     pot_rectangle_width = 12,
-    #     pot_rectangle_height = 10,
-    #     distance = 0,
-    #     lbls = paste0("rectangle12x10_", 1:17),
-    #     pot_type = "rectangle"
-    # ))
-    # return(PTR_generateBoardLayouts2(
-    #     pots = 17,
-    #     board_width = 30,
-    #     board_height = 60,
-    #     pot_rectangle_width = 10,
-    #     pot_rectangle_height = 12,
-    #     distance = 0,
-    #     lbls = paste0("rectangle10x12_", 1:17),
-    #     pot_type = "rectangle"
-    # ))
-
+test_that("getBoards2: fitting-equivalence for rectangular pots via rotation", {
+    ## ENFORCE_ORIENTATION = FALSE: Ensure that given equal input, both boards
+    ## get the same number of pots by flipping pot dimensions on one of them.
+    board30x60_rectpot12x10 <- get_boards2(30, 60, NA, NA, 12, 10, 17, "rectangle")
+    board30x60_rectpot10x12 <- get_boards2(30, 60, NA, NA, 10, 12, 17, "rectangle")
+    expect_true(length(board30x60_rectpot10x12$board_1$pot_centers) == length(board30x60_rectpot12x10$board_1$pot_centers))
+})
+test_that("getBoards2: forced-disabled orientation for rectangular pots via non-rotation", {
+    board30x60_rectpot12x10 <- get_boards2(30, 60, NA, NA, 12, 10, 17, "rectangle",TRUE)
+    board30x60_rectpot10x12 <- get_boards2(30, 60, NA, NA, 10, 12, 17, "rectangle",TRUE)
+    expect_true(length(board30x60_rectpot10x12$board_1$pot_centers) > length(board30x60_rectpot12x10$board_1$pot_centers))
 })
